2b54214b50215c3fcb4777c1eccd48c7a3121a80
fred1337 <fredm23@gmail.com>
2017-07-17 15:02:35 +0200

    modification, suppression documents (non fonctionnelles)

--- src/TF/ApiBundle/Controller/DocApiController.php
+++ src/TF/ApiBundle/Controller/DocApiController.php
@@ -39,7 +39,7 @@ Class DocApiController extends FOSRestController
         $repository = $this->getDoctrine()->getManager()->getRepository('TF\ApiBundle\Entity\tab_header_doc')->find($stamptab_header_doc);
         return $repository;
         //dump($repository);die;
-        $serializer = JMS::create()->build();
+        /*$serializer = JMS::create()->build();
         $serializer->serialize($repository, 'json');
         return array(
             'contact_name' => $repository->getContactName(),
@@ -57,7 +57,7 @@ Class DocApiController extends FOSRestController
             'doc_type' => $repository->getDocType(),
             'contact_city' => $repository->getContactCity()
 
-        );
+        );*/
     }
 
     /*
@@ -66,15 +66,65 @@ Class DocApiController extends FOSRestController
      */
     public function deleteDocAction($stamptab_header_doc)
     {
-        $repository = $this->getDoctrine()->getManager()->getRepository('TF\ApiBundle\Entity\tab_header_doc')->find($stamptab_header_doc);
+        $em = $this->getDoctrine()->getManager();
+
+        $doc = $em->getRepository('TF\ApiBundle\Entity\tab_header_doc')->find($stamptab_header_doc->get('stamptab_header_doc'));
         //delete le document qui correspond à l'id
-        $serializer = JMS::create()->build();
-        $serializer->serialize($repository, 'json');
+        if ($doc !== null) {
+            $em->remove($doc);
+            $em->flush();
+        }
     }
 
     //Modification d'un document
-    public function putDocAction()
+    /**
+     * @param Request $request
+     * @return Doc|View|Form
+     *
+     * @Rest\View()
+     * @Rest\Put(path="/games/{gameID}", requirements={"gameID" : "\d*"})
+     */
+    public function putDocAction(Request $request)
     {
-
+        return $this->handleDocData($request, false);
     }
+
+    /**
+     * @param Request $request
+     * @param bool $add
+     * @param bool $clearMissing
+     * @return Doc|View|Form
+     */
+    private function handleDocData(Request $request, $add = true, $clearMissing = true)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        if ($add) {
+            $doc = new tab_header_doc();
+        } else {
+            $doc = $em->getRepository('TF\ApiBundle\Entity\tab_header_doc')->find($request->get('stamptab_header_doc'));
+
+            if ($doc === null) {
+                return View::create(array('message' => 'This doc does not exists !'), Response::HTTP_NOT_FOUND);
+            }
+        }
+
+        $data = $request->request->all();
+        $data['file'] = $request->files->get('file');
+
+        $form = $this->createForm(DocType::class, $doc);
+        $form->submit($data, $clearMissing);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($add) {
+                $em->persist($doc);
+            }
+
+            $em->flush();
+            return $doc;
+        } else {
+            return $form;
+        }
+    }
+
 }
