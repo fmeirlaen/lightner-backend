ed4eb1263ec9a4b9b3969692fdfe6a300476d36d
fred1337 <fredm23@gmail.com>
2017-07-18 11:52:00 +0200

    formulaire contact

--- src/TF/ApiBundle/Controller/ContactApiController.php
+++ src/TF/ApiBundle/Controller/ContactApiController.php
@@ -6,24 +6,36 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use TF\ApiBundle\Entity\tab_contact;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use JMS\Serializer\SerializerBuilder as JMS;
 
 
 class ContactApiController extends FOSRestController
 {
     /**
      * @Rest\View()
+     * @Rest\Post(path="/addContact")
      */
     public function postContactAction(Request $request)
     {
-        $serializer = JMS::create()->build();
+        $doc = new tab_contact();
+        $form = $this->createForm(ContactType::class, $doc);
+        $form->submit($request->request->all());
+
+        if ($form->isValid() && $form->isSubmitted())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($doc);
+            $em->flush();
+            return $doc;
+        }
+        return $form;
+        /*$serializer = JMS::create()->build();
         $contact = $serializer->deserialize($request->getContent(), 'TF\ApiBundle\Entity\tab_contact', 'json');
 
         $em = $this->getDoctrine()->getManager();
         $country = $this->getDoctrine()->getRepository("TFApiBundle:tab_country")->find($contact->getCountryId());
         $contact->setStamptabCountry($country);
         $em->persist($contact);
-        $em->flush();
+        $em->flush();*/
 
     }
 
---
+++ src/TF/ApiBundle/Form/ContactType.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fred
+ * Date: 18/07/2017
+ * Time: 11:01
+ */
+
+namespace TF\ApiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use TF\ApiBundle\Entity\tab_contact;
+
+class ContactType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('contact_number');
+        $builder->add('contact_fs_name');
+        $builder->add('contact_ls_name');
+        $builder->add('contact_comercial_name');
+        $builder->add('contact_address');
+        $builder->add('contact_codpost');
+        $builder->add('contact_city');
+        $builder->add('contact_type');
+        $builder->add('contact_vat_number');
+        $builder->add('contact_amount');
+        $builder->add('contact_tab_vat');
+        $builder->add('contact_date_creation');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => tab_contact::class,
+            'csrf_protection' => false
+        ]);
+    }
+}
